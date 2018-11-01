46a1ebbef3041ccb48c90554d903cb8fe8a7ade1
戴晓波 <daixiaobo@wondersoft.cn>
2018-11-01 16:12:08 +0800

    zuul路由网关

---
+++ zuul/src/main/java/com/dxb/zuul/filter/AccessFilter.java
@@ -0,0 +1,36 @@
+package com.dxb.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+//    pre：路由之前
+//    routing：路由之时
+//    post： 路由之后
+//    error：发送错误调用
+    public String filterType() {
+        return "pre";
+    }
+
+    public int filterOrder() {
+        return 0;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            System.out.println("access token is  empty,setResponseStatusCode 401" );
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+        }
+        return null;
+    }
+}
