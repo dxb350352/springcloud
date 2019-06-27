898a37e8c9b94f03e3a63a116edf7e414599e1d0
戴晓波 <daixiaobo@wondersoft.cn>
2019-06-27 18:03:54 +0800

    spring boot admin

---
+++ adminserver/src/main/java/com/dxb/admin/AdminServerApplication.java
@@ -0,0 +1,72 @@
+package com.dxb.admin;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(AdminServerApplication.class, args);
+        System.out.println(context.getEnvironment().getProperty("spring.profiles.active") + ".......................");
+    }
+
+    /**
+     * @Profile激活指定配置文件：application.properties，相当于spring.profiles.active=dev
+     */
+    @Profile("insecure")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        static {
+            System.out.println("SecurityPermitAllConfig..............");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()//
+                    .and().csrf().disable();
+        }
+    }
+
+    @Profile("secure")
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        static {
+            System.out.println("SecuritySecureConfig..............");
+        }
+
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
+            // @formatter:on
+        }
+    }
+}
