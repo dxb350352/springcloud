2e93bd1831a14fb9618ee53af84faba6f415c3b7
戴晓波 <daixiaobo@wondersoft.cn>
2019-06-27 17:02:26 +0800

    zipkin优化

--- client/src/main/java/com/dxb/client/EurekaClientApplication222x.java
+++ client/src/main/java/com/dxb/client/EurekaClientApplication222x.java
@@ -3,7 +3,6 @@ package com.dxb.client;
 import com.dxb.client.controller.DiscoveryController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
--- zipkinclient/src/main/java/com/dxb/zipken/client/ZipkinClientApplication999x.java
+++ zipkinclient/src/main/java/com/dxb/zipken/client/ZipkinClientApplication999x.java
@@ -35,6 +35,27 @@ public class ZipkinClientApplication999x {
 
     @GetMapping("/zipadd")
     public String zipAdd(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
-        return restTemplate.getForEntity("http://eureka-client/add?a=" + a + "&b=" + b, String.class).getBody();
+        return restTemplate.getForEntity("http://zipkin-client/zipadd2?a=" + a + "&b=" + b, String.class).getBody();
+    }
+
+    @GetMapping("/zipadd2")
+    public String zipAdd2(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        String zipadd3 = restTemplate.getForEntity("http://zipkin-client/zipadd3?a=" + a + "&b=" + b, String.class).getBody();
+        String zipadd4 = restTemplate.getForEntity("http://zipkin-client/zipadd4?a=" + a + "&b=" + b, String.class).getBody();
+        if (zipadd3.equals(zipadd4)) {
+            return zipadd3;
+        }
+        return "error";
+    }
+
+    @GetMapping("/zipadd3")
+    public String zipAdd3(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        restTemplate.getForEntity("http://zipkin-client/zipadd4?a=" + a + "&b=" + b, String.class).getBody();
+        return String.valueOf(a + b);
+    }
+
+    @GetMapping("/zipadd4")
+    public String zipAdd4(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return String.valueOf(a + b);
     }
 }
