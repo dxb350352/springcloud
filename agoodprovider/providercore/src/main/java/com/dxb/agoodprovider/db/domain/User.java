70f94614533fc525ecae68fbcd9130db1e064152
戴晓波 <daixiaobo@wondersoft.cn>
2019-08-04 09:32:34 +0800

    小改

--- agoodprovider/providercore/src/main/java/com/dxb/agoodprovider/db/domain/User.java
+++ agoodprovider/providercore/src/main/java/com/dxb/agoodprovider/db/domain/User.java
@@ -5,7 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 /**
  * Created by dulei on 18/1/8.
@@ -21,4 +25,8 @@ public class User {
 
     private String username;
     private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createdTime;
 }
